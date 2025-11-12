60b04930275be108638e45357fd3e94c87b5e935
MALLIKHARJUNCH-02 <2k2cmk@gmail.com>
2025-11-12 21:37:20 +0530

    Added Notes Dsa, Java

--- Java/Java Codes/Learn.java
+++ Java/Java Codes/Learn.java
@@ -1,20 +1,61 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Learn {
 
-    public static int fact(int number) {
-        if ((number == 0) || (number == 1)) {
-            return 1;
+    public static boolean isPrime(int num){
+        if(num <=1) return false;
+
+        for(int i=2;i<=Math.sqrt(num);i++){
+            if(num%i==0){
+                return false;
+            }
         }
-        return number * fact(number - 1);
+
+        return true;
     }
-
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter the Number : ");
-        int n = input.nextInt();
-        System.out.println("The Factorial Of Number "+n+" is "+fact(n));
 
+        System.out.println("Enter The Number Of Rows : ");
+        int rows = input.nextInt();
+
+        System.out.println("Enter The Number Of Column : ");
+        int column = input.nextInt();
+
+        int[][] matrix = new int[rows][column];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < column; j++) {
+                System.out.println("Enter The Value Of The Cell [ " + i + " ][ " + j + " ]");
+                matrix[i][j] = input.nextInt();
+            }
+        }
+
+        int even = 0;
+        int odd = 0;
+        int prime = 0;
+        System.out.println("Final Matrix IS ");
+        for(int i= 0;i<rows;i++){
+            for(int j=0;j<column;j++){
+                System.out.print(matrix[i][j]+"  ");
+                
+                if(matrix[i][j]%2==0){
+                    even++;
+                }else{
+                    odd++;
+                }
+
+               int number = matrix[i][j];
+
+               if(isPrime(number)){
+                prime++;
+               }
+
+            }
+            System.out.println();
+        }
+
+        System.out.println("Total Number Of Even Numbers : [ "+even+" ] | Odd Numbers : [ "+odd+" ] | Prime Numbers : [ "+prime+" ]");
         input.close();
     }
 }
